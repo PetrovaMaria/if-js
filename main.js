6e73234af5a49932e587d34e76c7427b1ea5bfca
mashapetrova <masha00petrova@gmail.com>
2023-01-22 20:16:42 +0300

    made first and second task

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
-const text = 'hello world';
+const palindrome = function (word) {
+  for (let i = 0; i < word.length; ) {
+    if (word[i] === word[word.length - i - 1]) {
+      i++;
+      if (i === word.length) {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+};
 
-console.log(text);
+console.log(palindrome('шалаш'));
+
+console.log('----------');
+
+const min = function (a, b) {
+  if (a < b) {
+    return a;
+  }
+  return b;
+};
+
+const max = function (a, b) {
+  if (a > b) {
+    return a;
+  }
+  return b;
+};
+
+console.log(min(1, 2));
+console.log(max(1, 2));
+
+const min1 = function (a, b) {
+  return a < b ? a : b;
+};
+
+const max1 = function (a, b) {
+  return a > b ? a : b;
+};
+
+console.log(min1(3, 4));
+console.log(max1(3, 4));
+
+console.log('----------');
