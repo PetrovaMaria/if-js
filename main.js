4997452badeac8a8d351c1b9a663347eb2f11bc2
mashapetrova <masha00petrova@gmail.com>
2023-02-04 13:25:47 +0300

    made lesson4

--- main.js
+++ main.js
@@ -1,47 +1,27 @@
-let user = 'John Doe';
-console.log(user);
+export const sum = function (a) {
+  return function (b) {
+    return a + b;
+  };
+};
 
-const student = 'Masha';
-console.log(student);
+console.log('-----------------');
 
-user = student;
-console.log(user);
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+const text3 = document.getElementById('text3');
 
-let test = 1;
-test += 1;
-test += '1';
-console.log(test);
-test -= 1;
-console.log(test);
-test = Boolean(test);
-console.log(test);
+export const f1 = function () {
+  let k = 0;
+  return function () {
+    this.style.color = colors[k];
+    k++;
+    if (k === colors.length) {
+      k = 0;
+    }
+  };
+};
 
-const array = [2, 3, 5, 8];
-let result = 1;
-
-// eslint-disable-next-line for-direction
-for (let i = 0; i < array.length; i++) {
-  result *= array[i];
-}
-
-console.log(result);
-
-console.log('------------------');
-
-const array2 = [2, 5, 8, 15, 0, 6, 20, 3];
-
-for (let i = 0; i < array.length; i++) {
-  if (array2[i] > 5 && array2[i] < 10) {
-    console.log(array2[i]);
-  }
-}
-
-console.log('------------------');
-
-const array3 = [2, 5, 8, 15, 0, 6, 20, 3];
-
-for (let i = 0; i < array.length; i++) {
-  if (array3[i] % 2 === 0) {
-    console.log(array3[i]);
-  }
-}
+text1.addEventListener('click', f1());
+text2.addEventListener('click', f1());
+text3.addEventListener('click', f1());
