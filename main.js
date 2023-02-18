997e1b589982ba66f4122ed80b675b2631a41127
mashapetrova <masha00petrova@gmail.com>
2023-02-18 15:21:45 +0300

    learn arrays properties and used there.

--- main.js
+++ main.js
@@ -1,63 +1,266 @@
 const palindrome = function (word) {
-  for (let i = 0; i < word.length; ) {
-    if (word[i] === word[word.length - i - 1]) {
-      i++;
-      if (i === word.length) {
-        return true;
-      }
-    } else {
-      return false;
-    }
-  }
+  return word=== word.split('').reverse().join('') ? 'палиндром' : 'не палиндром';
 };
 
 console.log(palindrome('шалаш'));
 
 console.log('----------');
 
-const min = function (a, b) {
-  if (a < b) {
-    return a;
+const data = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
+
+data.forEach ( element => {
+  if (element.country.includes('Germany')) {
+    console.log(`${element.country}, ${element.city}, ${element.hotel} `);
   }
-  return b;
-};
-
-const max = function (a, b) {
-  if (a > b) {
-    return a;
-  }
-  return b;
-};
-
-console.log(min(1, 2));
-console.log(max(1, 2));
-
-const min1 = function (a, b) {
-  return a < b ? a : b;
-};
-
-const max1 = function (a, b) {
-  return a > b ? a : b;
-};
-
-console.log(min1(3, 4));
-console.log(max1(3, 4));
+});
 
 console.log('----------');
 
-const arr = [10, 2, 36, 15, 69, 100, 58, 78, 45, 60];
+const hotels = [
+  {
+    name: 'Hotel Leopold',
+    city: 'Saint Petersburg',
+    country: 'Russia',
+  },
+  {
+    name: 'Apartment Sunshine',
+    city: 'Santa Cruz de Tenerife',
+    country: 'Spain',
+  },
+  {
+    name: 'Villa Kunerad',
+    city: 'Vysokie Tatry',
+    country: 'Slowakia',
+  },
+  {
+    name: 'Hostel Friendship',
+    city: 'Berlin',
+    country: 'Germany',
+  },
+  {
+    name: 'Radisson Blu Hotel',
+    city: 'Kyiv',
+    country: 'Ukraine',
+  },
+  {
+    name: 'Paradise Hotel',
+    city: 'Guadalupe',
+    country: 'Mexico',
+  },
+  {
+    name: 'Hotel Grindewald',
+    city: 'Interlaken',
+    country: 'Switzerland',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+  },
+  {
+    name: 'Virgin Hotel',
+    city: 'Chicago',
+    country: 'USA',
+  },
+  {
+    name: 'Grand Beach Resort',
+    city: 'Dubai',
+    country: 'United Arab Emirates',
+  },
+  {
+    name: 'Shilla Stay',
+    city: 'Seoul',
+    country: 'South Korea',
+  },
+  {
+    name: 'San Firenze Suites',
+    city: 'Florence',
+    country: 'Italy',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+  },
+  {
+    name: 'Black Penny Villas',
+    city: 'BTDC, Nuca Dua',
+    country: 'Indonesia',
+  },
+  {
+    name: 'Koko Hotel',
+    city: 'Tokyo',
+    country: 'Japan',
+  },
+  {
+    name: 'Ramada Plaza',
+    city: 'Istanbul',
+    country: 'Turkey',
+  },
+  {
+    name: 'Waikiki Resort Hotel',
+    city: 'Hawaii',
+    country: 'USA',
+  },
+  {
+    name: 'Puro Hotel',
+    city: 'Krakow',
+    country: 'Poland',
+  },
+  {
+    name: 'Asma Suites',
+    city: 'Santorini',
+    country: 'Greece',
+  },
+  {
+    name: 'Cityden Apartments',
+    city: 'Amsterdam',
+    country: 'Netherlands',
+  },
+  {
+    name: 'Mandarin Oriental',
+    city: 'Miami',
+    country: 'USA',
+  },
+  {
+    name: 'Concept Terrace Hotel',
+    city: 'Rome',
+    country: 'Italy',
+  },
+  {
+    name: 'Ponta Mar Hotel',
+    city: 'Fortaleza',
+    country: 'Brazil',
+  },
+  {
+    name: 'Four Seasons Hotel',
+    city: 'Sydney',
+    country: 'Australia',
+  },
+  {
+    name: 'Le Meridien',
+    city: 'Nice',
+    country: 'France',
+  },
+  {
+    name: 'Apart Neptun',
+    city: 'Gdansk',
+    country: 'Poland',
+  },
+  {
+    name: 'Lux Isla',
+    city: 'Ibiza',
+    country: 'Spain',
+  },
+  {
+    name: 'Nox Hostel',
+    city: 'London',
+    country: 'UK',
+  },
+  {
+    name: 'Leonardo Vienna',
+    city: 'Vienna',
+    country: 'Austria',
+  },
+  {
+    name: 'Adagio Aparthotel',
+    city: 'Edinburgh',
+    country: 'UK',
+  },
+  {
+    name: 'Steigenberger Hotel',
+    city: 'Hamburg',
+    country: 'Germany',
+  },
+];
 
-for (let i = 0; i < arr.length; i++) {
-  let new_str = '';
-  const str = String(arr[i]);
-  for (let j = 0; j < str.length; j++) {
-    if (str[j] === '0') {
-      new_str = new_str + 'zero';
-    } else {
-      new_str = new_str + str[j];
-    }
-    arr[i] = new_str;
+const obj = {};
+
+hotels.forEach ( element => {
+  if(!obj[element.country]){
+    obj[element.country] = [];
   }
-}
+  obj[element.country].push(element.city);
+});
 
-console.log(arr);
+console.log(obj);
+
+console.log('----------');
+
+const getCalendarMonth = function (daysInMonth, daysInWeek, dayOfWeek){
+    const arr = [[]];
+    arr[0].fill(0);
+    let row = 0;
+      for ( let i = 0; i < dayOfWeek; i++){
+        arr[0][i] = daysInMonth - dayOfWeek + 1 + i;
+      }
+    for(let i=1; i<=daysInMonth; i++){
+      if(dayOfWeek<daysInWeek){
+        arr[row][dayOfWeek] = i;
+        dayOfWeek++;
+      }
+      else{
+        arr.push([]);
+        row += 1;
+        dayOfWeek = 0;
+        arr[row][dayOfWeek] = i;
+        dayOfWeek++;
+      }
+    }
+
+    let dayOfNextMonth = 1;
+    while(dayOfWeek<daysInWeek){
+      arr[arr.length - 1][dayOfWeek] = dayOfNextMonth;
+      dayOfNextMonth++;
+      dayOfWeek++;
+    }
+  return arr;
+};
+
+const daysInMonth = 30;
+const daysInWeek = 7;
+const dayOfWeek = 4;
+const calendarMonth = getCalendarMonth(daysInMonth, daysInWeek, dayOfWeek);
+
+console.log(calendarMonth);
