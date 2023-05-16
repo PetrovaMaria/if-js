d10dcb404cc63cd07ea09d30bff505f439175072
mashapetrova <masha00petrova@gmail.com>
2023-05-16 23:21:59 +0300

    added search button

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ const blocks = document.querySelector('.blocks');
 const results = document.querySelector('.results');
 const search_result = document.querySelector('.search-result');
 
-
 function AddItem(Element, HomesData) {
   HomesData.forEach((item) => {
     const article = document.createElement('article');
@@ -19,17 +18,14 @@ fetch('https://if-student-api.onrender.com/api/hotels/popular')
   .then((response) => response.json())
   .then((data) => AddItem(blocks, data.slice(0, 4)));
 
-
-
-
 const country = document.getElementById('places');
 const searchButton = document.getElementById('search-button');
 
 searchButton.addEventListener('click', () => {
   search_result.style.display = 'block';
-  fetch('https://if-student-api.onrender.com/api/hotels?search=' + country.value)
+  fetch(
+    'https://if-student-api.onrender.com/api/hotels?search=' + country.value,
+  )
     .then((response) => response.json())
     .then((result) => AddItem(results, result.slice(0, 4)));
 });
-
-
