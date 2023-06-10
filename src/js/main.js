d914db3b54f713153f59ade25aef83a15ddaa478
mashapetrova <masha00petrova@gmail.com>
2023-06-10 14:45:31 +0300

    add session storage

--- src/js/main.js
+++ src/js/main.js
@@ -1,74 +1,10 @@
-const data = [
-  {
-    id: '71ce9eac-e9b9-44f0-a342-9ff0b565f3b7',
-    name: 'Hotel Leopold',
-    city: 'Saint Petersburg',
-    country: 'Russia',
-    imageUrl:
-      'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg',
-  },
-  {
-    id: 'aa560608-a879-48a7-80b6-deff2806b250',
-    name: 'Apartment Sunshine',
-    city: 'Santa  Cruz de Tenerife',
-    country: 'Spain',
-    imageUrl:
-      'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg',
-  },
-  {
-    id: '1d88fefe-edf1-4cda-844a-babbf29bb2b3',
-    name: 'Villa Kunerad',
-    city: 'Vysokie Tatry',
-    country: 'Slowakia',
-    imageUrl:
-      'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg',
-  },
-  {
-    id: 'a2bf824d-edd8-41f0-8b70-244334086ab4',
-    name: 'Hostel Friendship',
-    city: 'Berlin',
-    country: 'Germany',
-    imageUrl:
-      'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
-  },
-  {
-    id: '4024535d-a498-4274-b7cb-f01ada962971',
-    name: 'Radisson Blu Hotel',
-    city: 'Kyiv',
-    country: 'Ukraine',
-    imageUrl:
-      'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg',
-  },
-  {
-    id: 'e51e71f6-6baf-4493-b3ae-25dc27cdc238',
-    name: 'Paradise Hotel',
-    city: 'Guadalupe',
-    country: 'Mexico',
-    imageUrl:
-      'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
-  },
-  {
-    id: '87d2b966-2431-43f3-8c0d-2c8723474dfc',
-    name: 'Hotel Grindewald',
-    city: 'Interlaken',
-    country: 'Switzerland',
-    imageUrl:
-      'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
-  },
-  {
-    id: '190221c6-b18f-4dba-97de-e35f0e14c023',
-    name: 'The Andaman Resort',
-    city: 'Port Dickson',
-    country: 'Malaysia',
-    imageUrl:
-      'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
-  },
-];
 
 const blocks = document.querySelector('.blocks');
-console.log(blocks);
+const results = document.querySelector('.results');
+const searchResult = document.querySelector('.Available-hotels');
+console.log(results);
 
-function AddItem(Element, HomesData) {
+function addItem(Element, HomesData) {
   HomesData.forEach((item) => {
     const article = document.createElement('article');
     article.innerHTML += `<article class="block js-loves-block">
@@ -80,15 +16,33 @@ function AddItem(Element, HomesData) {
   });
 }
 
-AddItem(blocks, data.slice(0, 4));
-
-/*
-data.map((element, index) => {
-  if (index < blocks.children.length) {
-    blocks.children[index].children[0].setAttribute('src', element.imageUrl);
-    blocks.children[index].children[1].innerHTML = element.name;
-    blocks.children[index].children[2].innerHTML =
-      element.city + ', ' + element.country;
+const request = function (results, Url) {
+  if(sessionStorage.getItem('url-address') === null){
+  fetch(
+    'https://if-student-api.onrender.com/api/hotels' + Url,
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      const data = result.slice(0, 4);
+        sessionStorage.setItem('url-address', JSON.stringify(data));
+        console.log(JSON.parse(sessionStorage.getItem('url-address')));
+        addItem(results, JSON.parse(sessionStorage.getItem('url-address')));
+    });
   }
+  else{
+    addItem(results, JSON.parse(sessionStorage.getItem('url-address')));
+  }
+};
+
+const country = document.getElementById('places');
+const searchButton = document.getElementById('search-button');
+
+const searchUrl = '?search=' + country.value;
+const Url = '/popular';
+request(blocks, Url);
+
+searchButton.addEventListener('click', () => {
+  searchResult.style.display = 'block';
+  request(results, searchUrl);
 });
-*/
+
