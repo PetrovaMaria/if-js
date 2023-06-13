9683fc5787b392df13e4668b5a566289bf3b0bea
mashapetrova <masha00petrova@gmail.com>
2023-06-13 21:36:13 +0300

    edited lesson-14 and add new document with variable

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,34 @@
-
-const blocks = document.querySelector('.blocks');
-const results = document.querySelector('.results');
-const searchResult = document.querySelector('.Available-hotels');
-console.log(results);
-
-function addItem(Element, HomesData) {
-  HomesData.forEach((item) => {
+import {urlAddress, fixUrl, getDataFromStorage, blocks, Url, searchUrl, results, searchResult, searchButton}  from './variable.js';
+function addItem(element, homesData) {
+  homesData.forEach((item) => {
     const article = document.createElement('article');
     article.innerHTML += `<article class="block js-loves-block">
     <img class="img-guests" src=${item.imageUrl} alt=${item.name} />
     <h3 class="hotel-title">${item.name}</h3>
     <p class="text-hotel">${item.city}, ${item.country}</p>
     </article>`;
-    Element.appendChild(article);
+    element.appendChild(article);
   });
 }
 
-const request = function (results, Url) {
-  if(sessionStorage.getItem('url-address') === null){
+const request = function (results, url) {
+  if(!sessionStorage.getItem(urlAddress)){
   fetch(
-    'https://if-student-api.onrender.com/api/hotels' + Url,
+    fixUrl + url,
   )
     .then((response) => response.json())
     .then((result) => {
       const data = result.slice(0, 4);
-        sessionStorage.setItem('url-address', JSON.stringify(data));
-        console.log(JSON.parse(sessionStorage.getItem('url-address')));
-        addItem(results, JSON.parse(sessionStorage.getItem('url-address')));
+        sessionStorage.setItem(urlAddress, JSON.stringify(data));
+        console.log(JSON.parse(getDataFromStorage));
+        addItem(results, JSON.parse(getDataFromStorage));
     });
   }
   else{
-    addItem(results, JSON.parse(sessionStorage.getItem('url-address')));
+    addItem(results, JSON.parse(getDataFromStorage));
   }
 };
 
-const country = document.getElementById('places');
-const searchButton = document.getElementById('search-button');
-
-const searchUrl = '?search=' + country.value;
-const Url = '/popular';
 request(blocks, Url);
 
 searchButton.addEventListener('click', () => {
---
+++ src/js/variable.js
@@ -0,0 +1,9 @@
+export const blocks = document.querySelector('.blocks');
+export const results = document.querySelector('.results');
+export const searchResult = document.querySelector('.Available-hotels');
+export const fixUrl = 'https://if-student-api.onrender.com/api/hotels';
+export const urlAddress = 'url-address';
+export const getDataFromStorage = sessionStorage.getItem(urlAddress);
+export const searchButton = document.getElementById('search-button');
+export const searchUrl = '?search=' + country.value;
+export const Url = '/popular';
